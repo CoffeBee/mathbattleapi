665f4d6c27f089d581c893edce186e4a6afc4d02
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2020-06-04 21:13:52 +0300

    add auth login

--- Sources/mathbattleapi/mathbattleapi.swift
+++ Sources/mathbattleapi/mathbattleapi.swift
@@ -2,16 +2,27 @@ import WebSocketKit
 import NIO
 
 public final class APIWebSocketController {
-    public init() {
+    
+    let token: String
+    let api_url: String
+    
+    public init(token: String, api_url: String = "api.math.silaeder.ru") {
+        self.token = token
+        self.api_url = api_url
     }
     public func run() throws {
         let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 2)
         let promise = eventLoopGroup.next().makePromise(of: String.self)
 
-        WebSocket.connect(to: "ws://127.0.0.1:8080/bot/ws", on: eventLoopGroup) { ws in
-            ws.send("HI")
+        WebSocket.connect(to: "ws://\(self.api_url)/bot/ws", on: eventLoopGroup) { ws in
+            ws.send(self.token)
             ws.onText { ws, text in
-                print(text)
+                if (text == "AUTH_FAILED") {
+                    print("Auth failed")
+                }
+                else {
+                    print("Auth success")
+                }
             }
         }.cascadeFailure(to: promise)
         _ = try promise.futureResult.wait()
